13be4961037fb51efe61d0d9d69bd5833d45a0b3
pearlKinn <jinju000202@naver.com>
2023-09-05 22:15:37 +0900

    :package: Chore: prettier 및 husky 세팅 완료

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,8 @@
 import useDocumentTitle from '@/hooks/useDocumentTitle';
 
-const Test = (e) => {
-  return <div>Home</div>;
-};
-
 function Home() {
   useDocumentTitle('TECHIT BY LIKELION');
-  function huskyTest() {
-    return '안녕';
-  }
+
   return (
     <div>
       <>허스키 테스트</>
