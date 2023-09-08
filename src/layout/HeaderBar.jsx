3841ee73fdd0533661115de24b0986dcdb3a961c
seobinbang7 <newmso5382@gmail.com>
2023-09-08 10:02:10 +0900

    커밋

---
+++ src/layout/HeaderBar.jsx
@@ -0,0 +1,13 @@
+import Heading from '@/components/Heading;';
+import Nav from './Nav';
+
+function HeaderBar() {
+  return (
+    <header>
+      <Heading />
+      <Nav />
+    </header>
+  )
+}
+
+export default HeaderBar;
